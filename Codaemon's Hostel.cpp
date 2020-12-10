4edacd9b3dea2864ac50bceaff282f73efd6507d
Code4All <75253091+code4allrgpv@users.noreply.github.com>
2020-12-10 15:58:22 +0530

    Create Codaemon's Hostel.cpp

---
+++ Codaemon's Hostel.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+// In this problem, we have to find the maximum horizontal and vertical
+// distance between the Corners of Main Hostel block and Meeting room block
+// Just maximize the sum of horizontal and vertical distance.
+ 
+int main() {
+    ll l,b,c,d,t;
+    cin>>t;
+    while(t-- > 0)
+    {
+        cin>>l>>b>>c>>d;
+        c = c-1;
+        d = d-1;
+        l = l-1;
+        b = b-1;
+        
+        ll ans = 0;
+        ans = max(ans, abs(0-c)+abs(0-d));
+        ans = max(ans, abs(0-c)+abs(b-d));
+        ans = max(ans, abs(l-c)+abs(0-d));
+        ans = max(ans, abs(l-c)+abs(b-d));
+        cout<<ans<<endl;
+    }
+    return 0;
+    
+}
