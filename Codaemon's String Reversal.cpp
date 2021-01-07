d245eb4dbc2ff4535ddb3da688847a3f9d71eeda
Code4All <75253091+code4allrgpv@users.noreply.github.com>
2021-01-07 20:42:51 +0530

    Create Codaemon's String Reversal.cpp

---
+++ Codaemon's String Reversal.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    char arr[n+1];
+    cin>>arr;
+    for(int i=n-1;i>=0;i--)
+    {
+        cout<<arr[i];
+    }
+    return 0;
+}
